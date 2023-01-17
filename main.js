7752408002b9d3bfd0251e314d04e15d2f0ff257
Ivy Nguyen <animeweirdo24@gmail.com>
2023-01-16 17:40:01 -0700

    start logic for classic mode

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
+let rockButton = document.getElementById("rockButtonInput")
+let paperButton = document.getElementById("paperButtonInput")
+let scissorButton = document.getElementById("scissorButtonInput")
+
+
+rockButton.addEventListener('click')
+paperButton.addEventListener('click')
+scissorButton.addEventListener('click')
+
+let player1 = new Player
 
 
 
+// {/* <script>
+//   let toggle = button => {
+//     let element = document.getElementById("mydiv");
+//     let hidden = element.getAttribute("hidden");
 
-
-
-
-{/* <script>
-  let toggle = button => {
-    let element = document.getElementById("mydiv");
-    let hidden = element.getAttribute("hidden");
-
-    if (hidden) {
-       element.removeAttribute("hidden");
-       button.innerText = "Hide div";
-    } else {
-       element.setAttribute("hidden", "hidden");
-       button.innerText = "Show div";
-    }
-  }
-</script> */}
+//     if (hidden) {
+//        element.removeAttribute("hidden");
+//        button.innerText = "Hide div";
+//     } else {
+//        element.setAttribute("hidden", "hidden");
+//        button.innerText = "Show div";
+//     }
+//   }
+// </script> */}
